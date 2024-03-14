d9b07d27d978278c0c0286c195da9d7881410289
Aman Kumar Maurya <amankrmj@gmail.com>
2024-03-14 09:41:00 +0530

    edited for multidex

--- android/app/src/main/kotlin/com/example/messenger_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/messenger_app/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.example.messenger_app
 
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import io.flutter.embedding.android.FlutterActivity
+
+class MainApplication : MultiDexApplication() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
 
 class MainActivity: FlutterActivity()
