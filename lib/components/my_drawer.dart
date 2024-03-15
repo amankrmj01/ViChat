e4d39bf2dcea836db4e9d11791c1ad3a4bdbe0f6
Aman Kumar Maurya <amankrmj@gmail.com>
2024-03-15 21:23:27 +0530

    updated drawer UI

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:messenger_app/components/my_logo.dart';
 import 'package:messenger_app/pages/settings.dart';
 
@@ -11,9 +9,18 @@ class DrawerC extends StatelessWidget {
   const DrawerC({super.key});
   @override
   Widget build(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).viewPadding.top;
+
+    // double? statusBarHeight = Get.statusBarHeight;
     List items = [
-      [const Text('Home'), Icons.home,(){Navigator.pop(context);}],
-      [const Text('Profile'), Icons.person_3_sharp,(){}],
+      [
+        const Text('Home'),
+        Icons.home,
+        () {
+          Navigator.pop(context);
+        }
+      ],
+      [const Text('Profile'), Icons.person_3_sharp, () {}],
       [
         const Text('Settings'),
         Icons.settings,
@@ -24,30 +31,33 @@ class DrawerC extends StatelessWidget {
       ],
     ];
     return Container(
-      padding: EdgeInsets.only(top: 26),
-      margin: EdgeInsets.zero,
+      padding: EdgeInsets.only(top: statusBarHeight ?? 36),
+      // margin: EdgeInsets.zero,
       child: Drawer(
         backgroundColor: Colors.amber.shade300,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const SizedBox(
+            Container(
               height: 75,
-              child: DrawerHeader(
-                  margin: EdgeInsets.zero,
-                  padding: EdgeInsets.only(left: 10, bottom: 4, top: 0),
-                  child: Row(
-                    children: [
-                      MyLogo(
-                        size: 0,
-                      ),
-                      Text(
-                        'Menu',
-                        style: TextStyle(fontSize: 30),
-                      )
-                    ],
-                  )),
+              padding: const EdgeInsets.all(5),
+              margin: EdgeInsets.zero,
+              alignment: Alignment.center,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  MyLogo(
+                    size: 0,
+                  ),
+                  SizedBox(width: 15,),
+                  Text(
+                    'Menu',
+                    style: TextStyle(fontSize: 30),
+                  )
+                ],
+              ),
             ),
+            Divider(thickness: 1,height: 0,color: Color.lerp(Colors.amber, Colors.black, 0.2),),
             Expanded(
               child: ListView.builder(
                   padding: EdgeInsets.all(5),
@@ -61,26 +71,27 @@ class DrawerC extends StatelessWidget {
                   }),
             ),
             Expanded(
-                child: Container(
-              alignment: Alignment.bottomCenter,
-              child: ElevatedButton(
-                onPressed: () async {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (builder) => const AuthGate()));
-                },
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.logout_rounded),
-                    Text('LogOut'),
-                  ],
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (builder) => const AuthGate()));
+                  },
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.logout_rounded),
+                      Text('LogOut'),
+                    ],
+                  ),
                 ),
               ),
-            ))
+            )
           ],
         ),
       ),
